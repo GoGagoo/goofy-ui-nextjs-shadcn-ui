8589c2ba61a3e6946dbb301d2bcbb31d6ebcf2d4
Gagik Antonyan <gagika57@gmail.com>
2023-08-16 18:00:53 +0500

    add user logout

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,8 +1,13 @@
+import UserNav from '@/components/UserNav'
 import React from 'react'
 
 function HomePage() {
 	return (
-		<div>HomePage</div>
+		<div className='flex-1 mt-10 mx-10'>
+			<div className="flex justify-end">
+				<UserNav />
+			</div>
+		</div>
 	)
 }
 
